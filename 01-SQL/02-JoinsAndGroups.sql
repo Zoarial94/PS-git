4045f6289f9fbb09bd1b527f11d4a94c03f88bf6
Hunter White <HWB94@protonmail.com>
2021-03-23 20:38:55 -0400

    Fixed Excercise 2 Problem 2
    I replaced buyPrice with MSRP to properly calculate the real sale price

--- 01-SQL/02-JoinsAndGroups.sql
+++ 01-SQL/02-JoinsAndGroups.sql
@@ -2,7 +2,7 @@ use classicmodels;
 
 SELECT c.customerName as "Customer Name", CONCAT(e.firstName, " ", e.lastName) as "Sales Rep" FROM customers c JOIN employees e ON c.salesRepEmployeeNumber = e.employeeNumber ORDER BY 1 ASC;
 
-SELECT p.productName as "Product Name", SUM(o.quantityOrdered) as "Total # Ordered", p.buyPrice * SUM(o.quantityOrdered) as "Total Sale" FROM products p JOIN orderdetails o ON p.productCode = o.productCode GROUP BY p.productCode ORDER BY 3 DESC; 
+SELECT p.productName as "Product Name", SUM(o.quantityOrdered) as "Total # Ordered", p.MSRP * SUM(o.quantityOrdered) as "Total Sale" FROM products p JOIN orderdetails o ON p.productCode = o.productCode GROUP BY p.productCode ORDER BY 3 DESC; 
 
 SELECT o.status as "Order Status", COUNT(*) as "# Orders" FROM orders o GROUP BY o.status ORDER BY o.status ASC;
 
